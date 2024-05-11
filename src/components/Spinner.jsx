afa33439254cfdf8d9349f22f3188b2c7686a677
Ankesh2004 <gankesh808@gmail.com>
2024-05-11 13:02:43 +0530

    Add spinner and display whe loading

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="dot dot1"></div>
+      <div className="dot dot2"></div>
+      <div className="dot dot3"></div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,25 @@
 import React, { useContext, useState } from "react";
 import { ProductContext } from "../contexts/ProductContext";
 import ProductCard from "../components/ProductCard";
+import Spinner from "../components/Spinner";
 
 interface ProductContextType {
   products: any[];
   allProducts: any[];
   setProducts: React.Dispatch<React.SetStateAction<any[]>>;
   filterProducts: (category: string) => void;
+  loadingProducts: boolean;
 }
 
 const Home = () => {
   // destructure from ProductContext
-  const { products, allProducts, setProducts, filterProducts,loadingProducts } =
-    useContext<ProductContextType>(ProductContext);
+  const {
+    products,
+    allProducts,
+    setProducts,
+    filterProducts,
+    loadingProducts,
+  } = useContext<ProductContextType>(ProductContext);
 
   // Filter products based on search text
   // Searchbar Text
@@ -50,21 +57,22 @@ const Home = () => {
       <h1 className="text-center font-bold lg:text-4xl text-2xl">
         Explore Our Products
       </h1>
-      <div className="flex flex-col sm:flex-row w-full">
-        <div className="flex w-[70%] justify-center mt-4">
+      <div className="flex flex-col justify-around sm:flex-row w-full">
+        <div className="flex justify-center mt-4 sm:w-[50%]">
           <input
             type="text"
             value={searchText}
             onChange={handleSearch}
             placeholder="Search Products"
-            className="w-full md:w-1/2 p-2 border border-gray-300 rounded-md"
+            className="w-full p-2 border border-gray-300 rounded-md"
           />
         </div>
-        <div className="flex justify-center w-[30%] mt-4">
+        <div className="flex justify-center items-center gap-1 mt-4 sm:w-[40%]">
+            <h2 className="font-bold md:text-xl sm:text-lg text-md">Category:</h2>
           <select
             value={category}
             onChange={handleCategory}
-            className="w-full md:w-1/2 p-2 border border-gray-300 rounded-md"
+            className="w-full p-2 border border-gray-300 rounded-md"
           >
             <option value="all">All</option>
             <option value="electronics">Electronics</option>
@@ -74,11 +82,15 @@ const Home = () => {
           </select>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-        {products.map((product: any) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      {loadingProducts ? (
+        <Spinner />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          {products.map((product: any) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
